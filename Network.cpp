32350e6bd73776f810e01c9160e3f4fb7c5257c0
MarcoP0520 <mperez53@horizon.csueastbay.edu>
2025-02-04 21:14:25 -0800

    Disconnections sent to status log

--- Network.cpp
+++ Network.cpp
@@ -18,42 +18,38 @@ bool Pingip(const std::string& ipAddress) {
 
 int main() {
 
-   std::vector<std::string> ipAddress_list{
-       "10.0.0.1",
-       "10.0.0.163",
-       "192.168.0.1",
-   };
+    std::vector<std::string> ipAddress_list{
+        "10.0.0.1",
+        "10.0.0.163",
+        "192.168.0.1",
+    };
     std::vector<std::string> devices{
         "Router1",
         "Switch1",
         "Test_Device",
     };
-
-
-    if(ipAddress_list.size() != devices.size()) {
-        std::cout << "Error: Number of IP addresses and Device mismatch!" << std::endl;
-    }
-    else {
-        while(true) {
-            std::ofstream log("status.log");
-            for(int pos = 0; pos < ipAddress_list.size(); pos++) {
-                std::string ip = ipAddress_list[pos];
-                std::string device = devices[pos];
-
-                bool status =  Pingip(ip);
-
-                if(status == false) {
-
-                    log << device + " " + ip + " Disconnected " << std::endl;
-                    log.flush();
-
-                }
-                else {
-                    std::cout <<device + " " + ip + " Connected" << std::endl;
-                }
-            }
-            log.close();
-            std::this_thread::sleep_for(std::chrono::seconds(10));
+    while(true) {
+        std::ofstream log("status.log", std::ios::app);
+        if(ipAddress_list.size() != devices.size()) {
+            std::cout << "Error: Number of IP addresses and Device mismatch!" << std::endl;
         }
+
+        for(int pos = 0; pos < ipAddress_list.size(); pos++) {
+            std::string ip = ipAddress_list[pos];
+            std::string device = devices[pos];
+
+            bool status =  Pingip(ip);
+
+            if(status == false) {
+
+                log << device << " " << ip << " Disconnected " << std::endl;
+            }
+            else {
+                std::cout <<device + " " + ip + " Connected" << std::endl;
+            }
+        }
+        log.close();
+        std::this_thread::sleep_for(std::chrono::seconds(10));
     }
+
 }
